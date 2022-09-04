cc143a1169cb0068877b32947a736c2ac9d4b7d3
Kev <kevinlewicki7@gmail.com>
2022-09-04 05:50:50 -0300

    cambio como llega la data del back

--- src/hooks/useGetItemDetail.js
+++ src/hooks/useGetItemDetail.js
@@ -2,18 +2,16 @@ import { useEffect, useState } from "react";
 
 function useGetItemDetail(id) {
   const [detail, setDetail] = useState([]);
-  const [description, setDescription] = useState("");
   useEffect(() => {
     setDetail(false)
     fetch(`/api/items?id=${id}`)
       .then((data) => data.json())
       .then((data) => {
-        setDetail(data[0]);
-        setDescription(data[1]);
+        setDetail(data);
       });
   }, [id]);
 
-  return [detail, description];
+  return [detail];
 }
 
 export { useGetItemDetail };
--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -7,7 +7,7 @@ import ImageDetails from "../../components/ImageDetails/ImageDetails";
 
 function Detail() {
   const { id } = useParams();
-  const [detail, description] = useGetItemDetail(id);
+  const [detail] = useGetItemDetail(id);
 
   return (
     <div className="container-article">
@@ -15,18 +15,18 @@ function Detail() {
         <article className="container-card-detail bg-white ">
           <div className="container-descripcion bg-white">
             <ImageDetails
-              pictures={detail.pictures}
-              thumbnail={detail.thumbnail}
+              pictures={detail.item?.pictures}
+              thumbnail={detail.item?.thumbnail}
             />
             <div className="bg-white descripcion-container">
               <h2 className="bg-white">Descripcion del producto</h2>
-              <p className="bg-white">{description.plain_text}</p>
+              <p className="bg-white">{detail.item?.plain_text}</p>
             </div>
           </div>
           <div className="bg-white container-title-card">
-            <h2 className="bg-white text-title">{detail.title}</h2>
+            <h2 className="bg-white text-title">{detail.item?.title}</h2>
             <div className="bg-white container-price-btn">
-              <h2 className="bg-white text-price">$ {detail.price}</h2>
+              <h2 className="bg-white text-price">$ {detail.item?.price}</h2>
               <button className="bg-white btn">Comprar</button>
             </div>
           </div>
